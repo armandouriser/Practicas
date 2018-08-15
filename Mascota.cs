42bcb0e4c4161145c167cf97ca6df9a84fa14195
Armando Uribe <armandouriser@hotmail.com>
2018-08-14 21:17:38 -0500

    Archivos

---
+++ Mascota.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perro
+{
+    class Mascota
+    {
+        public string Tamaño;
+        public string Raza;
+        public string Sexo;
+        public int peso;
+        public string Color;
+        public enum TIPOSDECOMIDA { CROQUETAS, BASURA, AGUA };
+
+        public Mascota()
+        {
+           Raza = "pitbull";
+           peso = 8;
+           Sexo = "Macho";
+           Tamaño = "grande";
+        }
+        public Mascota(string raza, int Peso, string sexo, string tamaño)
+        {
+            Raza = raza;
+            peso = Peso;
+            Sexo = sexo;
+            Tamaño = tamaño;
+        }
+
+        public void Ladrar()
+        {
+
+        }
+
+        public void Correr()
+        {
+
+        }
+        public bool Comer(int alimento)
+        {
+            bool resultado = false;
+            if (alimento == (int)TIPOSDECOMIDA.AGUA)
+            {
+                resultado = false;
+            }
+            else if (alimento == (int)TIPOSDECOMIDA.BASURA)
+            {
+                resultado = true;
+            }
+            else if (alimento == (int)TIPOSDECOMIDA.CROQUETAS)
+            {
+                resultado = true;
+            }
+            return resultado;
+
+        }
+        public void Cagar()
+        {
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perro
+{
+    class Perro
+
+    {
+        public enum TIPOSDECOMIDA { CROQUETAS, BASURA, AGUA };
+
+        static void Main(string[] args)
+        {
+            bool alimentado = false;
+            Mascota Perro1 = new Mascota();
+
+                Perro1.Raza = "French Puddle";
+                Perro1.peso = 7;
+                Perro1.Sexo = "Macho";
+                Perro1.Tamaño = "Pequeña";
+
+           alimentado = Perro1.Comer((int)TIPOSDECOMIDA.BASURA);
+            if (alimentado)
+            {
+                Console.WriteLine("el perro" + Perro1.Raza + "comio" + Convert.ToString(TIPOSDECOMIDA.BASURA));
+            }
+            else
+            {
+                Console.WriteLine("el perro no ha sido alimentado");
+            }
+
+            Console.Read();
+
+            Mascota Perro2 = new Mascota();
+            Console.WriteLine("el perro" + Perro2.Raza + "comio" + Convert.ToString(TIPOSDECOMIDA.BASURA));
+            Console.Read();
+
+            Mascota Perro3 = new Mascota("XD", 15, "hembra","pequeño");
+            Console.WriteLine("el perro" + Perro3.Raza + "comio" + Convert.ToString(TIPOSDECOMIDA.BASURA));
+            Console.Read();
+
+        }
+    }
+}
+
